54bbcf13491e24eeb4a0a0c73b9bf584a79def99
Darrell Tunnell <darrell.tunnell@googlemail.com>
2019-09-06 22:41:15 +0100

    Refactoring dep tree

--- src/Dazinator.Extensions.Permissions.Authorisation/ServiceCollectionExtensions.cs
+++ src/Dazinator.Extensions.Permissions.Authorisation/ServiceCollectionExtensions.cs
@@ -1,35 +1,15 @@
 ﻿using Dazinator.Extensions.Permissions;
-using Dazinator.Extensions.Permissions.Entities;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-
-        /// <summary>
-        /// Adds an <see cref="IPermissionStore{TApp, TAppPermission, TAppPermissionSubject, TAppPermissionType}"/> backed by a DbContext using the specified entity types. Use this if you have derived your own entities. 
-        /// </summary>
-        /// <typeparam name="TDbContext"></typeparam>
-        /// <typeparam name="TAppPermission"></typeparam>
-        /// <typeparam name="TAppPermissionType"></typeparam>
-        /// <typeparam name="TAppPermissionSubject"></typeparam>
-        /// <typeparam name="TApp"></typeparam>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static PermissionsRegistrationBuilder<TAppPermission, TAppPermissionType, TAppPermissionSubject, TApp> AddAuthorisationPolicyProvider<TAppPermission, TAppPermissionType, TAppPermissionSubject, TApp>(this PermissionsRegistrationBuilder<TAppPermission, TAppPermissionType, TAppPermissionSubject, TApp> builder)
-          where TAppPermissionType : IAppPermissionType, new()
-          where TAppPermission : class, IAppPermission<TAppPermission, TAppPermissionType>, new()
-          where TAppPermissionSubject : IAppPermissionSubject<TAppPermission, TAppPermissionType>, new()
-          where TApp : IApp<TAppPermissionSubject, TAppPermission, TAppPermissionType>, new()
-
+        public static IServiceCollection AddPermissionAuthorisationPolicyProvider<TAppPermission, TAppPermissionType, TAppPermissionSubject, TApp>(this IServiceCollection services)
         {
-            builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermissionsAuthorizationPolicyProvider>();
-            builder.Services.AddSingleton<PermissionPolicyNameProvider>();
-            return builder;
+            services.AddSingleton<IAuthorizationPolicyProvider, PermissionsAuthorizationPolicyProvider>();
+            services.AddSingleton<PermissionPolicyNameProvider>();
+            return services;
         }     
-
     }
 }
---
+++ src/Dazinator.Extensions.Permissions.Shared/CustomClaimTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace Dazinator.Extensions.Permissions
+{
+    public static class CustomClaimTypes
+    {
+        public const string Permission = "Application.Permission";
+    }
+  
+}
